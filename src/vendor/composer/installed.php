44c1de539e2c1fd94eca3acf3d407678587f414a
sn_netlag <derek@slow.ninja>
2024-12-11 17:56:23 -0500

    Added symfony/apache-pack to fix routing issue

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -566,6 +566,15 @@
             'aliases' => array(),
             'dev_requirement' => true,
         ),
+        'symfony/apache-pack' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '3aa5818d73ad2551281fc58a75afd9ca82622e6c',
+            'type' => 'symfony-pack',
+            'install_path' => __DIR__ . '/../symfony/apache-pack',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'symfony/asset' => array(
             'pretty_version' => 'v7.2.0',
             'version' => '7.2.0.0',
