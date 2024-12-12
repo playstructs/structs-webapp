1cf15a4e4b96dc856d8754820701693605b81369
sn_netlag <derek@slow.ninja>
2024-12-11 19:03:30 -0500

    Updated git ignore and added sample twig template

--- src/src/Controller/LuckyController.php
+++ src/src/Controller/LuckyController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,8 +13,8 @@ class LuckyController extends AbstractController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: ' . $number . '</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
--- src/var/cache/dev/App_KernelDevDebugContainer.php
+++ src/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerZtjr0Pn\App_KernelDevDebugContainer([
     'container.build_hash' => 'Ztjr0Pn',
-    'container.build_id' => 'e695cad9',
-    'container.build_time' => 1733957600,
+    'container.build_id' => '0dff67dc',
+    'container.build_time' => 1733961006,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerZtjr0Pn');
