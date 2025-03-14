d7531ff7dd33f73034e672f7e629ad0adab75193
sn_netlag <derek@slow.ninja>
2025-03-14 17:55:38 -0400

    Wallet creation test

--- src/js/view_models/signup/RecoveryKeyCreationViewModel.js
+++ src/js/view_models/signup/RecoveryKeyCreationViewModel.js
@@ -1,7 +1,37 @@
 import {MenuPage} from "../../framework/MenuPage";
+import {Instance} from "../../models/Instance";
+import {AbstractViewModel} from "../../framework/AbstractViewModel";
 
-export class RecoveryKeyCreationViewModel {
-  render() {
+export class RecoveryKeyCreationViewModel extends AbstractViewModel {
+
+  /**
+   * @param {string[]} mnemonicArray
+   * @return {string} html
+   */
+  renderRecoveryKeyHtml(mnemonicArray) {
+    let html = `<div id="recovery-key" class="text-recovery-key hidden">`;
+
+    for (let i = 0; i < mnemonicArray.length; i++) {
+      html += `
+      <div class="recovery-key-word">
+        <span class="sui-text-secondary">${i + 1}</span>
+        <span class="mod-white">${mnemonicArray[i]}</span>
+      </div>
+      `;
+    }
+
+    html += `</div>`;
+
+    return html;
+  }
+
+  async render() {
+    const instance = new Instance();
+    await instance.init();
+
+    const mnemonicArray = instance.mnemonic.split(' ');
+    const recoveryKeyHtml = this.renderRecoveryKeyHtml(mnemonicArray);
+
     MenuPage.hideAndClearNav();
 
     MenuPage.setBodyContent(`
@@ -28,56 +58,57 @@ export class RecoveryKeyCreationViewModel {
               <i class="sui-icon-md icon-key"></i>
               <span>Display Recovery Key</span>
             </a>
-            <div id="recovery-key" class="text-recovery-key hidden">
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">1</span>
-                <span class="mod-white">apple</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">2</span>
-                <span class="mod-white">mask</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">3</span>
-                <span class="mod-white">lens</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">4</span>
-                <span class="mod-white">scout</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">5</span>
-                <span class="mod-white">acid</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">6</span>
-                <span class="mod-white">exclude</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">7</span>
-                <span class="mod-white">evolve</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">8</span>
-                <span class="mod-white">double</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">9</span>
-                <span class="mod-white">build</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">10</span>
-                <span class="mod-white">theme</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">11</span>
-                <span class="mod-white">tone</span>
-              </div>
-              <div class="recovery-key-word">
-                <span class="sui-text-secondary">12</span>
-                <span class="mod-white">enlist</span>
-              </div>
-            </div>
+            ${ recoveryKeyHtml }
+<!--            <div id="recovery-key" class="text-recovery-key hidden">-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">1</span>-->
+<!--                <span class="mod-white">apple</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">2</span>-->
+<!--                <span class="mod-white">mask</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">3</span>-->
+<!--                <span class="mod-white">lens</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">4</span>-->
+<!--                <span class="mod-white">scout</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">5</span>-->
+<!--                <span class="mod-white">acid</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">6</span>-->
+<!--                <span class="mod-white">exclude</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">7</span>-->
+<!--                <span class="mod-white">evolve</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">8</span>-->
+<!--                <span class="mod-white">double</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">9</span>-->
+<!--                <span class="mod-white">build</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">10</span>-->
+<!--                <span class="mod-white">theme</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">11</span>-->
+<!--                <span class="mod-white">tone</span>-->
+<!--              </div>-->
+<!--              <div class="recovery-key-word">-->
+<!--                <span class="sui-text-secondary">12</span>-->
+<!--                <span class="mod-white">enlist</span>-->
+<!--              </div>-->
+<!--            </div>-->
           </div>
           <div>
             <button id= "written-down-btn" class="sui-screen-btn sui-mod-disabled" disabled>I've Written It Down</button>
