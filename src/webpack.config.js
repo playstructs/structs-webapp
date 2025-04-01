f390e7e0dcc3a78dafa57c807be2f063c13a25a0
sn_netlag <derek@slow.ninja>
2025-04-01 15:04:08 -0400

    structs-client-ts setup

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -15,6 +15,22 @@ module.exports = {
   experiments: {
     topLevelAwait: true,
   },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"]
+    }
+  },
+  module: {
+    rules: [
+      // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" , options: { transpileOnly: true } }
+    ]
+  },
   plugins: [
     new NodePolyfillPlugin()
   ]
