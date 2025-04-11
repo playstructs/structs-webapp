828f68dec464ccc9fbbb8ee64c2c383c1afe2957
sn_netlag <derek@slow.ninja>
2025-04-11 11:20:52 -0400

    Added ability to go back and restore a page to the MenuPageRouter

--- src/js/framework/MenuPageRouter.js
+++ src/js/framework/MenuPageRouter.js
@@ -1,6 +1,14 @@
 export class MenuPageRouter {
   constructor() {
     this.controllers = new Map();
+
+    this.currentController = null;
+    this.currentPage = null;
+    this.currentOptions = {};
+
+    this.lastController = null;
+    this.lastPage = null;
+    this.lastOptions = {};
   }
 
   registerController(controller) {
@@ -8,6 +16,45 @@ export class MenuPageRouter {
   }
 
   goto(controllerName, pageName, options = {}) {
+    this.lastController = this.currentController;
+    this.lastPage = this.currentPage;
+    this.lastOptions = this.currentOptions;
+
+    this.currentController = controllerName;
+    this.currentPage = pageName;
+    this.currentOptions = options;
+
+    localStorage.setItem("lastMenuPage", JSON.stringify({
+      controller: this.lastController,
+      page: this.lastPage,
+      options: this.lastOptions
+    }))
+    localStorage.setItem("currentMenuPage", JSON.stringify({
+      controller: controllerName,
+      page: pageName,
+      options: options
+    }));
+
     this.controllers.get(controllerName)[pageName](options);
   }
+
+  back() {
+    this.goto(this.lastController, this.lastPage, this.lastOptions);
+  }
+
+  restore() {
+    const lastMenuPage = JSON.parse(localStorage.getItem("lastMenuPage"));
+    const currentMenuPage = JSON.parse(localStorage.getItem("currentMenuPage"));
+
+    if (!currentMenuPage || !lastMenuPage) {
+      throw new Error("No stored menu page to restore from.");
+    }
+
+    this.currentPage = lastMenuPage.page;
+    this.currentController = lastMenuPage.controller;
+    this.currentOptions = lastMenuPage.options;
+
+    this.goto(currentMenuPage.controller, currentMenuPage.page, currentMenuPage.options);
+  }
+
 }
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1143,6 +1143,14 @@ __webpack_require__.r(__webpack_exports__);
 class MenuPageRouter {
   constructor() {
     this.controllers = new Map();
+
+    this.currentController = null;
+    this.currentPage = null;
+    this.currentOptions = {};
+
+    this.lastController = null;
+    this.lastPage = null;
+    this.lastOptions = {};
   }
 
   registerController(controller) {
@@ -1150,8 +1158,47 @@ class MenuPageRouter {
   }
 
   goto(controllerName, pageName, options = {}) {
+    this.lastController = this.currentController;
+    this.lastPage = this.currentPage;
+    this.lastOptions = this.currentOptions;
+
+    this.currentController = controllerName;
+    this.currentPage = pageName;
+    this.currentOptions = options;
+
+    localStorage.setItem("lastMenuPage", JSON.stringify({
+      controller: this.lastController,
+      page: this.lastPage,
+      options: this.lastOptions
+    }))
+    localStorage.setItem("currentMenuPage", JSON.stringify({
+      controller: controllerName,
+      page: pageName,
+      options: options
+    }));
+
     this.controllers.get(controllerName)[pageName](options);
   }
+
+  back() {
+    this.goto(this.lastController, this.lastPage, this.lastOptions);
+  }
+
+  restore() {
+    const lastMenuPage = JSON.parse(localStorage.getItem("lastMenuPage"));
+    const currentMenuPage = JSON.parse(localStorage.getItem("currentMenuPage"));
+
+    if (!currentMenuPage || !lastMenuPage) {
+      throw new Error("No stored menu page to restore from.");
+    }
+
+    this.currentPage = lastMenuPage.page;
+    this.currentController = lastMenuPage.controller;
+    this.currentOptions = lastMenuPage.options;
+
+    this.goto(currentMenuPage.controller, currentMenuPage.page, currentMenuPage.options);
+  }
+
 }
 
 /***/ }),
