e41ec2bbbb68ca08a8b3c09c561d5220b3f86f4f
abstrct <abstrct@slow.ninja>
2025-11-05 12:51:49 -0500

    Basic Hashing Task models

---
+++ src/js/models/TaskState.js
@@ -0,0 +1,16 @@
+
+export class TaskState {
+  constructor() {
+    this.prefix = null; // Entire string up to NONCE
+    this.postfix = null; // Optional IDENTITY
+    this.nonce_start = null;
+    this.nonce_current = null;
+    this.difficulty_start = null;
+    this.difficulty_current = null;
+    this.block_start = null;
+    this.result_hash = null;
+    this.result_nonce = null;
+    this.created_at = null;
+    this.updated_at = null;
+  }
+}
