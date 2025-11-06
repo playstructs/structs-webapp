89109f048dbf03e56124c027dbc012c52f82da0b
abstrct <abstrct@slow.ninja>
2025-11-05 21:42:53 -0500

    Continued tweaking basic Task system. Adding some constants  and making processes accessible across the app.

---
+++ src/js/constants/TaskStatus.js
@@ -0,0 +1,8 @@
+export const TASK_STATUS = {
+  NEW: 'new',
+  RUNNING: 'running',
+  PAUSING: 'pausing',
+  PAUSED: 'paused',
+  TERMINATED: 'terminated',
+  COMPLETED: 'completed',
+};
--- src/js/models/TaskProcess.js
+++ src/js/models/TaskProcess.js
@@ -1,31 +1,65 @@
+import {TASK} from "../constants/TaskConstants";
+import {TASK_STATUS} from "../constants/TaskStatus";
+import {TASK_MESSAGE_TYPES} from "../constants/TaskMessageTypes";
 
 export class TaskProcess {
   constructor() {
     this.state = null;
     this.worker = null;
-    this.status = null;
+    this.status = TASK_STATUS.NEW;
+
+    this.success_callback = null;
   }
 
   hasWorker() {
+    return (this.worker !== undefined) && (this.worker !== null) && (this.worker !== "");
+  }
 
+  isNew() {
+    return this.status === TASK_STATUS.NEW;
   }
 
   isRunning() {
-
+    return this.status === TASK_STATUS.RUNNING;
   }
 
-  startWorker() {
+  isPaused() {
+    return this.status === TASK_STATUS.PAUSED;
+  }
+
+  isTerminated() {
+    return this.status === TASK_STATUS.TERMINATED;
+  }
+
+  start() {
     if(!this.hasWorker()) {
-      // Setup Worker
+      this.worker = new Worker(TASK.WORKER_PATH);
+
+      // TODO
+      // Deal with the Results handler
+      this.worker.onmessage = async function (result) {
+        console.log('Received from action worker [Process ID #' + result.data[0].id + ']...');
+      }
+
+      // Send the initial state to the Worker
+      this.worker.postMessage([this.state]);
     }
     this.worker.start()
+
+    this.status = TASK_STATUS.RUNNING;
   }
 
-  stopWorker() {
-
+  pause() {
+    this.worker.postMessage([TASK_MESSAGE_TYPES.PAUSE_AND_EXPORT]);
+    this.status = TASK_STATUS.PAUSING;
   }
 
-  sendMessage() {
+  terminate() {
+    this.worker.terminate()
+    this.status = TASK_STATUS.TERMINATED;
+  }
 
+  sendMessage(msg) {
+    this.worker.postMessage([msg]);
   }
 }
