b050a4834822c666d8f920ceed6b96487314e456
sn_netlag <derek@slow.ninja>
2025-12-17 15:14:30 -0500

    Split simple tile and tile row rendering into a separate class for reuse

--- src/js/view_models/components/map/MapStructLayerComponent.js
+++ src/js/view_models/components/map/MapStructLayerComponent.js
@@ -1,17 +1,12 @@
-import {AbstractViewModelComponent} from "../../../framework/AbstractViewModelComponent";
-import {
-  MAP_COL_ATTACKER_COMMAND, MAP_COL_ATTACKER_FLEET, MAP_COL_DEFENDER_COMMAND, MAP_COL_DEFENDER_FLEET,
-  MAP_COL_DEFENDER_PLANETARY, MAP_COL_DIVIDER, MAP_DEFAULT_FLEET_COL_COUNT,
-  MAP_TILE_ROWS_PER_AMBIT, MAP_TILE_TYPES,
-  MAP_DEFAULT_COMMAND_COL_COUNT
-} from "../../../constants/MapConstants";
+import {MAP_TILE_TYPES} from "../../../constants/MapConstants";
 import {EVENTS} from "../../../constants/Events";
 import {StructStillBuilder} from "../../../builders/StructStillBuilder";
 import {Player} from "../../../models/Player";
 import {Struct} from "../../../models/Struct";
+import {GenericMapLayerComponent} from "./GenericMapLayerComponent";
 
 
-export class MapStructLayerComponent extends AbstractViewModelComponent {
+export class MapStructLayerComponent extends GenericMapLayerComponent {
 
   /**
    * @param {GameState} gameState
@@ -33,30 +28,20 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
     isRaidMap = false,
     containerId = ""
   ) {
-    super(gameState);
-    this.structManager = structManager;
-    this.mapColBreakdown = mapColBreakdown;
-    this.dividerIndex = this.mapColBreakdown.lastIndexOf(MAP_COL_DIVIDER);
-    this.planet = planet;
-    this.defender = defender;
-    this.attacker = attacker;
+    super(
+      gameState,
+      'map-struct-layer-row',
+      'map-struct-layer-tile',
+      structManager,
+      mapColBreakdown,
+      planet,
+      defender,
+      attacker
+    );
+
     this.isRaidMap = isRaidMap;
     this.containerId = containerId;
     this.structStillBuilder = new StructStillBuilder(this.gameState);
-  }
-
-  /**
-   * @param {number} col
-   * @return {string}
-   */
-  getTileSide(col) {
-    if (col < this.dividerIndex) {
-      return 'left';
-    } else if (col === this.dividerIndex) {
-      return '';
-    } else {
-      return 'right';
-    }
   }
 
   /**
@@ -88,8 +73,8 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
    */
   getStructTileClass(struct) {
     return (struct && struct.is_building)
-      ? 'map-struct-layer-tile map-struct-layer-tile-building'
-      : 'map-struct-layer-tile';
+      ? `${this.tileClass} map-struct-layer-tile-building`
+      : this.tileClass;
   }
 
   /**
@@ -105,7 +90,7 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
   /**
    * Determine tile type from struct location and type
    * @param {Struct} struct
-   * @return {string}
+   * @return {string|null}
    */
   getTileTypeFromStruct(struct) {
     if (struct.location_type === 'planet') {
@@ -164,7 +149,7 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
    * Get location info for a tile based on its type and player
    * @param {string} tileType
    * @param {string} playerId
-   * @return {{locationType: string, locationId: string, isCommandSlot: boolean}|null}
+   * @return {{locationType: string, locationId: string|null, isCommandSlot: boolean}|null}
    */
   getLocationInfoFromTile(tileType, playerId) {
     if (tileType === MAP_TILE_TYPES.PLANETARY_SLOT) {
@@ -235,316 +220,6 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
   }
 
   /**
-   * @param {string} tileType the tile type. See MAP_TILE_TYPES constant array.
-   * @param {string} side the side of the map the tile is on
-   * @param {string} playerId the ID of the player that owns the tile or empty if no one does such as a transition tile.
-   * @param {string} ambit the ambit the tile is in or empty if it's a transition tile.
-   * @param {string|number} slot the planetary or fleet slot number. Empty if it's not a command, planetary, fleet or command tile.
-   * @return {string}
-   */
-  renderStructTileHTML(
-    tileType,
-    side = "",
-    playerId = "",
-    ambit = "",
-    slot = ""
-  ) {
-    return `
-      <div
-        class="map-struct-layer-tile"
-        data-tile-type="${tileType}"
-        data-side="${side}"
-        data-player-id="${playerId}"
-        data-ambit="${ambit}"
-        data-slot="${slot}"
-      ></div>
-    `;
-  }
-
-  renderFogOfWarTileHTML(mapColType) {
-    if (this.attacker) {
-      return '';
-    }
-
-    const mapColTypeLastIndex = this.mapColBreakdown.lastIndexOf(mapColType);
-    const dividerIndex = this.mapColBreakdown.lastIndexOf(MAP_COL_DIVIDER);
-    const attackerSide = (this.mapColBreakdown[0] === MAP_COL_ATTACKER_COMMAND) ? 'LEFT' : 'RIGHT';
-
-    if (dividerIndex === -1 || mapColTypeLastIndex === -1) {
-      throw new Error('Divider or map col type not found');
-    }
-
-    if (
-      mapColType === MAP_COL_DIVIDER
-      || (attackerSide === 'RIGHT' && dividerIndex < mapColTypeLastIndex)
-      || (attackerSide === 'LEFT' && mapColTypeLastIndex < dividerIndex)
-    ) {
-      return this.renderStructTileHTML(
-        MAP_TILE_TYPES.FOG_OF_WAR,
-        attackerSide.toLowerCase()
-      );
-    }
-
-    return '';
-  }
-
-  /**
-   * @param {string} topAmbit the ambit that is on top in the transition
-   * @param {string} bottomAmbit the ambit that is on the bottom in the transition
-   * @param {boolean} isInFinalTransitionPosition is this for the final transition of the map
-   * @param {number|null} totalAmbits the total number of ambits in the ambit
-   * @param {number|null} bottomAmbitIndex the ambit index of the bottom ambit
-   * @return {string} the row of struct tiles for the whole transition row
-   */
-  renderTransitionRowHTML(
-    topAmbit,
-    bottomAmbit,
-    isInFinalTransitionPosition = false,
-    totalAmbits = null,
-    bottomAmbitIndex= null
-  ) {
-    const isFinalTransition = isInFinalTransitionPosition && (bottomAmbitIndex === (totalAmbits - 1));
-
-    if (isInFinalTransitionPosition && !isFinalTransition) {
-      return '';
-    }
-
-    let tiles = '';
-
-    for (let c = 0; c < this.mapColBreakdown.length; c++) {
-      tiles += this.renderFogOfWarTileHTML(this.mapColBreakdown[c])
-        || this.renderStructTileHTML(
-          MAP_TILE_TYPES.TRANSITION,
-          this.getTileSide(c)
-        );
-    }
-
-    return `
-      <div class="map-struct-layer-row">
-        ${tiles}
-      </div>
-    `;
-  }
-
-  /**
-   * Creates a command slot tracker object for an ambit.
-   * Each command column type gets 1 usable slot per ambit.
-   *
-   * @return {Object} commandSlotTracker
-   */
-  createCommandSlotTracker() {
-    return {
-      [MAP_COL_DEFENDER_COMMAND]: MAP_DEFAULT_COMMAND_COL_COUNT,
-      [MAP_COL_ATTACKER_COMMAND]: MAP_DEFAULT_COMMAND_COL_COUNT,
-    };
-  }
-
-  /**
-   * @param {string} mapColType
-   * @param {string} side
-   * @param {string} ambit
-   * @param {Object} commandSlotTracker
-   * @return {string}
-   */
-  renderCommandTileHTML(
-    mapColType,
-    side,
-    ambit,
-    commandSlotTracker
-  ) {
-    let playerId = '';
-
-    if (mapColType === MAP_COL_DEFENDER_COMMAND) {
-      playerId = this.defender.id;
-    } else if (mapColType === MAP_COL_ATTACKER_COMMAND) {
-      playerId = this.attacker.id;
-    } else {
-      return '';
-    }
-
-    // Check if there's an available command slot for this column type
-    const hasAvailableSlot = commandSlotTracker[mapColType] > 0;
-
-    if (hasAvailableSlot) {
-      commandSlotTracker[mapColType]--;
-    }
-
-    const tileType = hasAvailableSlot
-      ? MAP_TILE_TYPES.COMMAND
-      : MAP_TILE_TYPES.COMMAND_BLOCKED;
-
-    // Command structs are always slot 0 in a fleet
-    return this.renderStructTileHTML(
-      tileType,
-      side,
-      playerId,
-      ambit,
-      hasAvailableSlot ? 0 : ''
-    );
-  }
-
-  /**
-   * @param {string} mapColType
-   * @param {string} side
-   * @param {string} ambit
-   * @param {string} slot
-   * @return {string}
-   */
-  renderPlanetaryTileHTML(
-    mapColType,
-    side,
-    ambit,
-    slot
-  ) {
-    if (mapColType !== MAP_COL_DEFENDER_PLANETARY) {
-      return '';
-    }
-
-    const tileType = (slot === '')
-      ? MAP_TILE_TYPES.PLANETARY_BLOCKED
-      : MAP_TILE_TYPES.PLANETARY_SLOT;
-
-    return this.renderStructTileHTML(
-      tileType,
-      side,
-      this.defender.id,
-      ambit,
-      slot
-    );
-  }
-
-  /**
-   * @param {string} mapColType
-   * @param {string} side
-   * @param {string} ambit
-   * @param {string} slot
-   * @return {string}
-   */
-  renderDefenderFleetTileHTML(
-    mapColType,
-    side,
-    ambit,
-    slot
-  ) {
-    if (mapColType !== MAP_COL_DEFENDER_FLEET) {
-      return '';
-    }
-
-    return this.renderStructTileHTML(
-      MAP_TILE_TYPES.FLEET,
-      side,
-      this.defender.id,
-      ambit,
-      slot
-    );
-  }
-
-  /**
-   * @param {string} mapColType
-   * @param {string} side
-   * @param {string} ambit
-   * @param {string} slot
-   * @return {string}
-   */
-  renderAttackerFleetTileHTML(
-    mapColType,
-    side,
-    ambit,
-    slot
-  ) {
-    if (mapColType !== MAP_COL_ATTACKER_FLEET) {
-      return '';
-    }
-
-    return this.renderStructTileHTML(
-      MAP_TILE_TYPES.FLEET,
-      side,
-      this.attacker.id,
-      ambit,
-      slot
-    );
-  }
-
-  /**
-   * @param {string} mapColType
-   * @param {string} ambit
-   * @return {string}
-   */
-  renderDividerTileHTML(
-    mapColType,
-    ambit
-  ) {
-    if (mapColType !== MAP_COL_DIVIDER) {
-      return '';
-    }
-
-    return this.renderStructTileHTML(
-      MAP_TILE_TYPES.DIVIDER,
-      '',
-      '',
-      ambit
-    );
-  }
-
-  /**
-   * @param {string} targetColType
-   * @return {{first: null|number, last: null|number}}
-   */
-  findFirstAndLastOccurrencesOfColType(targetColType) {
-    let firstOccurrence = null;
-    let lastOccurrence = null;
-
-    for(let i = 0; i < this.mapColBreakdown.length; i++) {
-
-      if (this.mapColBreakdown[i] !== targetColType) {
-        continue;
-      }
-
-      if (firstOccurrence === null) {
-        firstOccurrence = i;
-      }
-
-      lastOccurrence = i;
-    }
-
-    return {
-      first: firstOccurrence,
-      last: lastOccurrence
-    }
-
-  }
-
-  /**
-   * @param {string} targetColType
-   * @param {number} currentMapRowIndex
-   * @param {number} currentMapColIndex
-   * @param {number} totalSlots
-   * @return {string}
-   */
-  calcSlotNumber(
-    targetColType,
-    currentMapRowIndex,
-    currentMapColIndex,
-    totalSlots
-  ) {
-    let targetColTypeOccurrences = this.findFirstAndLastOccurrencesOfColType(targetColType);
-
-    const tilesOfTargetTypePerRow = (targetColTypeOccurrences.last - targetColTypeOccurrences.first) + 1;
-
-    // Slot indexing from right to left
-    const slotsToReserveThisRow = (targetColTypeOccurrences.last - currentMapColIndex);
-    let slotNumber = slotsToReserveThisRow + currentMapRowIndex * tilesOfTargetTypePerRow;
-
-    // Slot indexing from left to right
-    if (this.mapColBreakdown[0] === MAP_COL_ATTACKER_COMMAND) {
-      const slotsAssignedThisRow = currentMapColIndex - targetColTypeOccurrences.first;
-      slotNumber = slotsAssignedThisRow + currentMapRowIndex * tilesOfTargetTypePerRow;
-    }
-
-    return (slotNumber >= totalSlots) ? '' : `${slotNumber}`;
-  }
-
-  /**
    * Initialize page code: populate structs and set up event listeners
    */
   initPageCode() {
@@ -564,73 +239,5 @@ export class MapStructLayerComponent extends AbstractViewModelComponent {
         this.renderStruct(event.struct);
       }
     });
-  }
-
-  /**
-   * @return {string}
-   */
-  renderHTML() {
-    let html = '';
-    let previousAmbit = '';
-
-    const planetAmbits = this.planet.getAmbits();
-
-    for (let a = 0; a < planetAmbits.length; a++) {
-
-      const currentAmbit = planetAmbits[a];
-      const numPlanetarySlots = this.planet.getPlanetarySlotsByAmbit(currentAmbit, this.gameState.structTypes);
-      const totalFleetSlotsPerAmbitPerPlayer = MAP_TILE_ROWS_PER_AMBIT * MAP_DEFAULT_FLEET_COL_COUNT;
-      const commandSlotTracker = this.createCommandSlotTracker();
-
-      html += this.renderTransitionRowHTML(previousAmbit, currentAmbit);
-
-      for (let r = 0; r < MAP_TILE_ROWS_PER_AMBIT; r++) {
-
-        html += `<div class="map-struct-layer-row">`;
-
-        for (let c = 0; c < this.mapColBreakdown.length; c++) {
-
-          const mapColType = this.mapColBreakdown[c];
-          let side = this.getTileSide(c);
-
-          html += this.renderFogOfWarTileHTML(mapColType)
-            || this.renderCommandTileHTML(mapColType, side, currentAmbit, commandSlotTracker)
-            || this.renderPlanetaryTileHTML(
-              mapColType,
-              side,
-              currentAmbit,
-              this.calcSlotNumber(MAP_COL_DEFENDER_PLANETARY, r, c, numPlanetarySlots)
-            )
-            || this.renderDefenderFleetTileHTML(
-              mapColType,
-              side,
-              currentAmbit,
-              this.calcSlotNumber(MAP_COL_DEFENDER_FLEET, r, c, totalFleetSlotsPerAmbitPerPlayer)
-            )
-            || this.renderAttackerFleetTileHTML(
-              mapColType,
-              side,
-              currentAmbit,
-              this.calcSlotNumber(MAP_COL_ATTACKER_FLEET, r, c, totalFleetSlotsPerAmbitPerPlayer)
-            )
-            || this.renderDividerTileHTML(mapColType, currentAmbit)
-          ;
-        }
-
-        html += `</div>`;
-      }
-
-      html += this.renderTransitionRowHTML(
-        previousAmbit,
-        currentAmbit,
-        true,
-        planetAmbits.length,
-        a
-      );
-
-      previousAmbit = currentAmbit;
-    }
-
-    return html;
   }
 }
